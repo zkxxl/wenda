cfcd577904d8ad3f72d3a349c8a096ba9a838b94
阿凯160508 <904621245@qq.com>
2018-09-23 15:17:09 +0800

    测试

---
+++ src/main/java/com/z/kwenda/WendaApplication.java
@@ -0,0 +1,13 @@
+package com.z.kwenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WendaApplication {
+
+    public static void main(String[] args) {
+        System.out.print("nihao");
+        SpringApplication.run(WendaApplication.class, args);
+    }
+}
