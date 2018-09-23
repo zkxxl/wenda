43c6ae4bcbc4ab5a48ef3d7ed6625a856b35ed55
阿凯160508 <904621245@qq.com>
2018-09-23 15:26:10 +0800

    恩

--- src/main/java/com/z/kwenda/WendaApplication.java
+++ src/main/java/com/z/kwenda/WendaApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WendaApplication {
 
     public static void main(String[] args) {
-        System.out.print("nihao");
         SpringApplication.run(WendaApplication.class, args);
     }
 }
