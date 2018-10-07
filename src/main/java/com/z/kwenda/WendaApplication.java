ac2b8a1c60955e7c5ffe9bb46b0a630662b90e01
阿凯160508 <904621245@qq.com>
2018-10-07 13:43:53 +0800

    测试下

--- src/main/java/com/z/kwenda/WendaApplication.java
+++ src/main/java/com/z/kwenda/WendaApplication.java
@@ -3,10 +3,16 @@ package com.z.kwenda;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class WendaApplication {
 
+    public static void print(int index,Object object){
+        System.out.println(String.format("{%d}, %s",index,object.toString()));
+    }
     public static void main(String[] args) {
         SpringApplication.run(WendaApplication.class, args);
     }
+
 }
